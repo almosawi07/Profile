77be2acea76d524c262817ef336fc1de80f8b277
Sayed hassan <88848951+SayedHsn69@users.noreply.github.com>
2022-03-02 20:04:51 +0300

    Create profile.js
    my profile

---
+++ profile.js
@@ -0,0 +1,11 @@
+```js
+const profile = {
+    name:"Sayed-hassan",
+    age:15,
+    Free-time:("Programming","Playing","watching tvshows/anime","reading")
+   };
+   
+   console.log(profile);
+
+
+```
